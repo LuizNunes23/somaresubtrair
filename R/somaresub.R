0430c46450271a56a7b3ac2ac28eef1900e8ad0e
Luiz Nunes 23 <lpns@discente.ifpe.edu.br>
2021-08-27 10:42:20 -0300

    first commit

---
+++ R/somaresub.R
@@ -0,0 +1,19 @@
+#'Função somar ou subtrair
+#'
+#'Essa função serve para somar ou subtrair dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' somar(3,4)
+#' subtrair(3,4)
+#'
+#' @export
+somar <- function(a, b){
+  a + b
+}
+
+subtrair <- function(a, b){
+  a - b
+}
